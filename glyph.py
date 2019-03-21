230d06970fc055fbb37bafccfcb13417c70fecf3
Dr Yann Golanski <yann.golanski@neverness.org>
2019-03-21 11:51:19 +0000

    Added glyphs.

---
+++ glyph.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# http://www.fileformat.info/info/unicode/char/search.htm
+
+lst = [u"\u263A",  # ☺
+       u"\u2368",  # ⍨
+       u'\u2639',  # ☹
+       u'\u2622',  # ☢
+       u'\u2623',  # ☣
+       u'\u2620',  # ☠
+       u'\u2744',  # ❄
+       u'\u26A0',  # ⚠
+       u'\u2192',  # →
+       u'\u2190',  # ←
+       u"\u2191",  # ↑
+       u"\u2193",  # ↓
+       u"\u2665",  # ♥
+       u"\u00DC",  # Ü
+       u"\u00FC",  # ü
+       u"死",      # Death / shi
+       ]
+for c in lst:
+    print(u'  {} '.format(c))
